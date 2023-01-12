cdf6d75d40e163a19fbc5fe296392f2f3e44cd7c
Hiyawnew Haile <hiyawnewhaile@gmail.com>
2023-01-12 11:58:54 -0500

    update

--- components/About.tsx
+++ components/About.tsx
@@ -13,7 +13,7 @@ export default function About({ pageInfo }: Props) {
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1.5 }}
-            className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'
+            className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl p-10 justify-evenly mx-auto items-center'
         >
             <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
                 About
@@ -30,7 +30,7 @@ export default function About({ pageInfo }: Props) {
             whileInView={{ opacity: 1, x:0 }}
             viewport={{ once:true }}
             src={urlFor(pageInfo.profilePic).url()}
-            className='-mb-20 md:mb-0 flex-shrink-0 w-56 h56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]'
+            className='md:mb-0 flex-shrink-0 w-56 h56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]'
             />
 
             <div className='space-y-10 px-0 md:px-10'>
--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -25,8 +25,8 @@ export default function ExperienceCard({ experience }: Props) {
 
         <div className='px-0 md:px-10'>
             <h4 className='text-4xl font-light'> {experience.jobTitle} </h4>
-            <p className='font-bold text-2xl mt-1'>Weather App</p>
-            <div className='flex space-x-2 my-2'>
+            <p className='font-bold text-2xl mt-1'> {experience.company} </p>
+            <div className='flex space-x-2 my-2 flex-wrap'>
                 {experience.technologies.map(technology => (
                     <img 
                         key={technology._id}
